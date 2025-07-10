7767db194f1d3967b61caf17468a2f12a26955c3
rivenintech <contact@rivenintech.com>
2025-07-10 12:38:27 +0200

    feat: create database client

--- src/lib/graphql/schema.ts
+++ src/lib/graphql/schema.ts
@@ -1,13 +1,5 @@
-import * as dbSchema from "@/db/schema";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+import { db } from "@/db/client";
 import { buildSchema } from "drizzle-graphql";
-import { drizzle } from "drizzle-orm/neon-http";
 import { GraphQLSchema } from "graphql";
-
-config({ path: ".env" });
-
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql, schema: dbSchema });
 
 export const schema: GraphQLSchema = buildSchema(db).schema;
